c2fc51e66d5e196fb2219b794d6519b07cb2663f
FYELIC <84096391+fyelic@users.noreply.github.com>
2025-07-08 13:23:25 -0400

    Create tiltSensorCode.py

---
+++ examples/tiltSensor/tiltSensorCode.py
@@ -0,0 +1,20 @@
+"""
+Tilt Sensor Test
+Modified 8 July 2025
+By Caroline Vooss
+
+Purpose: Test a simple circuit with a tilt sensor and LED
+
+Attributes: http://multiwingspan.co.uk/pico.php?page=tilt
+"""
+
+from machine import Pin
+from time import sleep
+
+tilt = Pin(0, Pin.IN, Pin.PULL_UP)
+
+led = Pin(1, Pin.OUT)
+
+while True:
+    led.value(tilt.value())
+    sleep(1)
