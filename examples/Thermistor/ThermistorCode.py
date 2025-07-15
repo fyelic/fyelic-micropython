be53db6cb420d9f9e8d433da6883a87a6d4f3c43
FYELIC <84096391+fyelic@users.noreply.github.com>
2025-07-15 09:13:54 -0400

    Update ThermistorCode.py

--- examples/Thermistor/ThermistorCode.py
+++ examples/Thermistor/ThermistorCode.py
@@ -14,11 +14,11 @@ import math
 adc = machine.ADC(26) #Pin thermistor is connected to
 
 while True:
-    adcValue = adc.read_u16() #Measures the voltage going through the thermistor
-    voltage = adcValue / 65535.0 * 3.3 #ADC (analog-to-digital converter) value is proportional to the voltage passed through
-    Rt = 10 * voltage / (3.3 - voltage) #Rt is the resistance measured in the new temperature
-    tempK = (1 / (1 / (273.15 + 25) + (math.log(Rt / 10)) / 3950)) #This equation is used to find the temperature in Kelvin
-    tempC = int(tempK - 273.15) #This equation is used to find the temperature in Celsius
+    adcValue = adc.read_u16()                                         #Measures the voltage going through the thermistor
+    voltage = adcValue / 65535.0 * 3.3                                #ADC (analog-to-digital converter) value is proportional to the voltage passed through
+    Rt = 10 * voltage / (3.3 - voltage)                               #Rt is the resistance measured in the new temperature
+    tempK = (1 / (1 / (273.15 + 25) + (math.log(Rt / 10)) / 3950))    #This equation is used to find the temperature in Kelvin
+    tempC = int(tempK - 273.15)                                       #This equation is used to find the temperature in Celsius
     print("ADC Value: ", adcValue, "Voltage: %0.2f"%voltage,"Temperature: ",tempC,"C")
     time.sleep(1)
     
