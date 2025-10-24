6d69c048328ead4267278202710eea80ff639c82
FYELIC <84096391+fyelic@users.noreply.github.com>
2025-10-24 15:45:29 -0400

    Create rfid-example.py

---
+++ examples/RFID/rfid-example.py
@@ -0,0 +1,28 @@
+'''
+Modified: 24 October 2025
+By Jordan Spencer
+Adapted from Microcontrollers Lab
+
+Purpose: Read the ID of a RFID card/tag
+Notes: One library needs to be installed prior to running code:
+Look for [mfrc522.py]and save to Raspberry Pi.
+'''
+
+from mfrc522 import MFRC522
+import utime
+
+reader = MFRC522(spi_id=0,sck=2,miso=4,mosi=3,cs=1,rst=0)
+
+print("Bring TAG closer...")
+print("")
+
+
+while True:
+    reader.init()
+    (stat, tag_type) = reader.request(reader.REQIDL)
+    if stat == reader.OK:
+        (stat, uid) = reader.SelectTagSN()
+        if stat == reader.OK:
+            card = int.from_bytes(bytes(uid),"little",False)
+            print("CARD ID: "+str(card))
+utime.sleep_ms(500) 
